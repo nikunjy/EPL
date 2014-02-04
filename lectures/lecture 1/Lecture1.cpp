273dc3b145b3acc20d38160921645ecf3d7f6552
nikunj <nikunj@cs.utexas.edu>
2014-02-04 16:11:38 -0600

    Changing cpp file little bit

--- lectures/lecture 1/Lecture1.cpp
+++ lectures/lecture 1/Lecture1.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
+/* 
+	This example shows you : 
+	 1. Use of static member inside the class. 
+	 2. A member function can access the private parts of any object. Even if called from the scope of other object. 
+	 3. 2nd is true because member function actually isn't located in memory of one object but common to a class. 
+*/
 class Foo { 
 	private: 
 		static int x; 
